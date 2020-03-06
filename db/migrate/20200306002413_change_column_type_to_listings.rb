6784bed36d9dae3d160a3ab7fd9dea8943af1d84
Zhihao Zhang <z.zhihao@columbia.edu>
2020-03-06 17:38:39 -0500

    compatible for sqlite creating

--- db/migrate/20200306002413_change_column_type_to_listings.rb
+++ db/migrate/20200306002413_change_column_type_to_listings.rb
@@ -2,5 +2,7 @@ class ChangeColumnTypeToListings < ActiveRecord::Migration
   def change
     change_column :listings, :daily_price, "decimal USING daily_price::numeric(15,2)"
     change_column :listings, :size, "decimal USING daily_price::numeric(15,2)"
+    # change_column :listings, :daily_price, :decimal, precision: 15, scale: 2
+    # change_column :listings, :size,  :decimal, precision: 15, scale: 2
   end
 end
