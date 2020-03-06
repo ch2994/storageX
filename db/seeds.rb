7128d077125751a60fdba34b9f3c607151343d88
Cheng Huang <ch2994@columbia.edu>
2020-03-05 20:06:46 -0500

    Clears db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create([{userid: '000001',username: 'nymei', password: '123456'} ])
 
-listings = [{:storage_id => 'sx101', :address => '155 W 68th St', :zipcode => '10023', :daily_price => 20, :email => 'abc@columbia.edu'},
-            {:storage_id => 'sx102', :address => '240 Park Ave S', :zipcode => '10003', :daily_price => 30, :email => 'def@columbia.edu'},
-            {:storage_id => 'sx103', :address => '10 Hanover Sq', :zipcode => '10005', :daily_price => 40, :email => 'hij@columbia.edu'}
-]
-
-listings.each do |listing|
-  Listing.create!(listing)
-end
 
