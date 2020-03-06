c105e26c6dae5d9a4f2c1f7a672d99d9aa3fcada
Zhihao Zhang <z.zhihao@columbia.edu>
2020-03-05 19:40:04 -0500

    Patch Listing model function - user_filter and change listing table column type

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -34,7 +34,7 @@ class Listing < ActiveRecord::Base
       return self.all
     elsif conditions.nil? or conditions.empty?
       return self.order(sorted_col.to_sym)
-    elsif (sorted_col.nil? or not @valid_sort_cols.include?sorted_col)
+    elsif (sorted_col.nil? or not @valid_sort_cols.include?sorted_col.to_sym)
       return self.where(:zipcode => conditions["zipcode"])
     else
       return self.where(:zipcode => conditions["zipcode"]).order(sorted_col)
