06181440da9f759ad146f80847b7a098645895dc
Zhihao Zhang <z.zhihao@columbia.edu>
2020-03-06 14:51:16 -0500

    Fix migration conflict

--- db/migrate/20200305190755_create_customers.rb
+++ db/migrate/20200305190755_create_customers.rb
@@ -1,6 +1,5 @@
 class CreateCustomers < ActiveRecord::Migration
   def change
-    drop_table :customers
     create_table :customers do |t|
       t.string :username
       t.string :password_digest
