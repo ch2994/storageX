f5814d7a61c2ea5de8f5de5064f7a4eba64bec7b
niuyinmei <jeff.mei@outlook.com>
2020-03-23 18:40:42 -0400

    working on editing

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,6 +10,7 @@ class BookingsController < ApplicationController
   # GET /bookings/1
   # GET /bookings/1.json
   def show
+    debugger
   end
 
   # GET /bookings/new
@@ -24,6 +25,10 @@ class BookingsController < ApplicationController
 
   # GET /bookings/1/edit
   def edit
+    debugger
+    @booking = Booking.find(params[:id])
+    @listing = Listing.find(@booking.listing_id)
+    debugger
   end
 
   # POST /bookings
@@ -48,8 +53,16 @@ class BookingsController < ApplicationController
   # PATCH/PUT /bookings/1
   # PATCH/PUT /bookings/1.json
   def update
-    @temp = Booking.new(booking_params)
+    @temp = set_booking
+    @temp[:start_date] = DateTime.new(booking_params["start_date(1i)"].to_i,
+                                      booking_params["start_date(2i)"].to_i,
+                                      booking_params["start_date(3i)"].to_i)
+    @temp[:end_date] = DateTime.new(booking_params["end_date(1i)"].to_i,
+                                      booking_params["end_date(2i)"].to_i,
+                                      booking_params["end_date(3i)"].to_i)
+    @temp[:updated_at] = DateTime.now
     respond_to do |format|
+      debugger
       if Booking.validate(booking_params) and Booking.time_checking(@temp) and @booking.update(booking_params)
         format.html { redirect_to @booking, notice: 'Booking was successfully updated.' }
         format.json { render :show, status: :ok, location: @booking }
@@ -80,6 +93,9 @@ class BookingsController < ApplicationController
     def booking_params
       params_new = params.require(:booking).permit(:start_date, :end_date)
       params_new[:listing_id] = params[:listing_id].to_i
+      if params_new[:listing_id] == 0
+          params_new[:listing_id] = Booking.find(params[:id]).listing_id
+      end
       params_new[:customer_id] = session[:customer_id]
       params_new
     end
