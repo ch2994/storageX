758e2131e4a31f624bf4516ba67a8ab38c88d61f
Zhihao Zhang <z.zhihao@columbia.edu>
2020-04-02 12:23:56 -0400

    Drop invalid users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200331195555) do
+ActiveRecord::Schema.define(version: 20200402162141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,12 +57,6 @@ ActiveRecord::Schema.define(version: 20200331195555) do
   create_table "reviews", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "userid"
-    t.string "password"
   end
 
   add_foreign_key "bookings", "customers"
