2117a85d3672e0c2be41c8889e51b9f4cefc2c5e
Zhihao Zhang <z.zhihao@columbia.edu>
2020-04-18 16:29:47 -0400

    Fix functional bugs.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -34,6 +34,13 @@ class ListingsController < ApplicationController
     @all_listings = Listing.user_filter(conditions, sorted_col, search_query)
     if not cur_customer_id.nil?
       @all_listings = @all_listings.where(:customer_id => cur_customer_id)
+      @all_listings = @all_listings.to_a.map!(&:serializable_hash)
+      @all_listings.collect do |one_listing|
+        one_listing['rating'] = Review.where(:listing_id => one_listing['id']).average("rating")
+      end
+      if sorted_col == 'rating'
+        @all_listings.sort!{|a,b| a['rating'] && b['rating'] ? a['rating'] <=> b['rating'] : a['rating'] ? -1 : 1}
+      end
     else
       redirect_to login_path
     end
@@ -42,8 +49,13 @@ class ListingsController < ApplicationController
 
   def show
     id = params[:id] # retrieve movie ID from URI route
-    @listing = Listing.find(id) # look up movie by unique ID
-    @all_reviews = Review.where(:listing_id => params[:id])
+    @listing = Listing.find_by(:id=>id) # look up movie by unique ID
+    if @listing.nil?
+      flash[:notice] = "The listing doesn't existing"
+      redirect_to listings_path
+    else
+      @all_reviews = Review.where(:listing_id => params[:id])
+    end
     # will render app/views/movies/show.<extension> by default
   end
 
@@ -81,20 +93,18 @@ class ListingsController < ApplicationController
 
   def edit
     gon.azure_map_key = azure_map_key
-    @listing = set_listing
     if cur_customer_id != @listing.customer_id
       redirect_to listings_index_path, notice: 'Unauthorized access to the Edit page of others\' listing.'
     end
   end
 
   def update
-    @temp = set_listing
     listing_params[:updated_at] = DateTime.now
     respond_to do |format|
       updated_listing = listing_params
       specific_address = [updated_listing['address'], updated_listing['city'], updated_listing['state'], updated_listing['zipcode']].join(', ')
       updated_listing = updated_listing.merge(Listing.get_long_lat_by_address(specific_address))
-      if cur_customer_id != @temp.customer_id
+      if cur_customer_id != @listing.customer_id
         # Prevent illegal update others' listings
         format.html { redirect_to listings_index_path, notice: 'Unauthorized Edit Operation was banned.' }
         format.json { render json: @listing.errors, status: :unprocessable_entity }
@@ -110,7 +120,17 @@ class ListingsController < ApplicationController
   end
 
   def destroy
-
+    if Listing.find(params[:id]).customer_id != cur_customer_id
+      # Prevent illegal update others' listings
+      format.html { redirect_to listings_index_path, notice: 'Unauthorized Delete Operation was banned.' }
+      format.json { render json: @listing.errors, status: :unprocessable_entity }
+    elsif Listing.delete(params[:id])
+      flash[:notice] = 'Successfully deleted the designated listing.'
+      redirect_to show_mine_path(cur_customer_id)
+    else
+      flash[:notice] = 'Listing Delete operation failed. Please check your own listing again.'
+      format.html { redirect_to show_mine_path(cur_customer_id) }
+    end
   end
 
   def show_review
@@ -137,7 +157,11 @@ class ListingsController < ApplicationController
     end
 
     def set_listing
-      @listing = Listing.find(params[:id])
+      @listing = Listing.find_by(:id => params[:id])
+      if @listing.nil?
+        flash[:notice] ="The listing doesn't exist"
+        redirect_to listings_path
+      end
     end
 
     def cur_customer_id
